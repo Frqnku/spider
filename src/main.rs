3ec365d2366f8b0f8419f7aa6d6d9412b3d91c8c
Frqnku <ugo.tiberto@gmail.com>
2025-08-11 17:32:15 +0200

    feat: added multithreaded recursion to scrape images deeply with performances

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,36 @@
 mod download;
+mod extract_url;
 mod parse_args;
 mod rebuild_url;
-mod scraper;
+mod scrape;
+use std::{collections::HashSet, sync::Arc};
+
 use parse_args::Cli;
+use tokio::sync::{Mutex, Semaphore};
+
+use crate::scrape::scrape_and_download;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::new();
-    println!("{cli:?}");
+
     if let Err(err) = cli.check_path() {
         eprintln!("{err}");
         std::process::exit(1);
     }
 
-    let response = reqwest::get(&cli.url)
-        .await
-        .map_err(|e| format!("Failed to fetch URL: {e}"))?
-        .text()
-        .await
-        .map_err(|e| format!("Failed to read response text: {e}"))?;
+    let semaphore = Arc::new(Semaphore::new(10)); // Limite 10 tâches simultanées
+    let visited = Arc::new(Mutex::new(HashSet::new()));
 
-    let image_urls: Vec<String> = scraper::extract_image_urls(&cli.url, &response)
-        .map_err(|e| format!("Failed to extract image URLs: {e}"))?;
-
-    if let Err(e) = download::download_images(image_urls, &cli.path).await {
-        eprintln!("Error downloading images: {e}");
-        std::process::exit(1);
-    }
-
-    let deeper_urls: Vec<String> = scraper::extract_deeper_urls(&cli.url, &response)
-        .map_err(|e| format!("Failed to extract deeper URLs: {e}"))?;
-
-    for url in deeper_urls {
-        println!("Found deeper URL: {url}");
-    }
+    scrape_and_download(
+        cli.url,
+        cli.path,
+        cli.recursive,
+        cli.limit + 1,
+        semaphore,
+        visited,
+    )
+    .await?;
 
     Ok(())
 }
---
+++ src/scrape.rs
@@ -0,0 +1,74 @@
+use std::{collections::HashSet, path::PathBuf, sync::Arc};
+
+use futures::future::{BoxFuture, FutureExt};
+use tokio::sync::{Mutex, Semaphore};
+
+use crate::{download, extract_url};
+
+pub fn scrape_and_download(
+    url: String,
+    download_path: PathBuf,
+    recursive: bool,
+    limit: usize,
+    semaphore: Arc<Semaphore>,
+    visited: Arc<Mutex<HashSet<String>>>,
+) -> BoxFuture<'static, Result<(), String>> {
+    async move {
+        {
+            let mut visited_lock = visited.lock().await;
+            if visited_lock.contains(&url) {
+                return Ok(());
+            }
+            visited_lock.insert(url.clone());
+        }
+
+        if limit == 0 {
+            return Ok(());
+        }
+
+        let _permit = semaphore.acquire().await.map_err(|e| e.to_string())?;
+
+        let response = reqwest::get(&url)
+            .await
+            .map_err(|e| format!("Failed to fetch URL {}: {}", &url, e))?
+            .text()
+            .await
+            .map_err(|e| format!("Failed to read response text for {}: {}", &url, e))?;
+
+        let image_urls = extract_url::extract_image_urls(&url, &response)?;
+        download::download_images(image_urls, download_path.as_ref()).await?;
+
+        if recursive {
+            let deeper_urls = extract_url::extract_deeper_urls(&url, &response)?;
+
+            let mut handles = Vec::new();
+
+            for deeper_url in deeper_urls {
+                let download_path_clone = download_path.clone();
+                let semaphore_clone = semaphore.clone();
+                let visited_clone = visited.clone();
+                let limit_clone = limit - 1;
+                dbg!(&deeper_url);
+
+                let fut = scrape_and_download(
+                    deeper_url,
+                    download_path_clone,
+                    recursive,
+                    limit_clone,
+                    semaphore_clone,
+                    visited_clone,
+                );
+
+                let handle = tokio::spawn(fut);
+                handles.push(handle);
+            }
+
+            for handle in handles {
+                let _ = handle.await;
+            }
+        }
+
+        Ok(())
+    }
+    .boxed()
+}
